0d3464b475d44dca25f982d496de2a07d89d0288
Tony Sharples <tonysharples22@me.com>
2024-01-18 17:25:03 +0000

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Tony Sharples on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
