83553251f39522726d4ae75d4b45ffbc1dcd0cae
Tony Sharples <tonysharples22@me.com>
2024-01-19 14:03:45 +0000

    Create form and generate questions

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -7,15 +7,60 @@
 
 import SwiftUI
 
+struct Question {
+    let multiplicationTable: String
+    let question: String
+    let answer: String
+}
+
 struct ContentView: View {
+    @State private var multiplicationTable = 2
+    @State private var selectedNumberOfQuestions = "5"
+    @State private var selectedDifficulty = "Easy"
+    @State private var questions = [Question]()
+    
+    var numberOfQuestionsOptions = ["5", "10", "20"]
+    var difficultyOptions = ["Easy", "Medium", "Hard"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Stepper(value: $multiplicationTable, in: 2...12) {
+                    Text("\(multiplicationTable) times tables")
+                }
+                
+                Picker("Number of questions", selection: $selectedNumberOfQuestions) {
+                    ForEach(numberOfQuestionsOptions, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                Picker("Difficulty", selection: $selectedDifficulty) {
+                    ForEach(difficultyOptions, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            
+            Section {
+                Button("Start") {
+                    startGame()
+                }
+            }
         }
-        .padding()
+        .onAppear(perform: generateQuestions)
+    }
+    
+    func startGame() {
+        // TODO: Display the questions based on the selected options.
+    }
+    
+    func generateQuestions() {
+        for table in 1...12 {
+            for number in 1...20 {
+                questions.append(Question(multiplicationTable: "\(table)", question: "\(number) x \(table)", answer: "\(number * table)"))
+            }
+        }
     }
 }
 
