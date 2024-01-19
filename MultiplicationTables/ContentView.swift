9fe76b0d03bded76575ecc3d8face9a1f9f07417
Tony Sharples <tonysharples22@me.com>
2024-01-19 16:40:44 +0000

    Create questions NavigationStack

--- MultiplicationTables/ContentView.swift
+++ MultiplicationTables/ContentView.swift
@@ -15,44 +15,74 @@ struct Question {
 
 struct ContentView: View {
     @State private var multiplicationTable = 2
-    @State private var selectedNumberOfQuestions = "5"
+    @State private var selectedNumberOfQuestions = 5
     @State private var selectedDifficulty = "Easy"
     @State private var questions = [Question]()
+    @State private var gameIsActive = true
+    @State private var answer = ""
+    @State private var questionNumber = 0
     
     var numberOfQuestionsOptions = ["5", "10", "20"]
     var difficultyOptions = ["Easy", "Medium", "Hard"]
     
     var body: some View {
-        Form {
-            Section {
-                Stepper(value: $multiplicationTable, in: 2...12) {
-                    Text("\(multiplicationTable) times tables")
-                }
-                
-                Picker("Number of questions", selection: $selectedNumberOfQuestions) {
-                    ForEach(numberOfQuestionsOptions, id: \.self) {
-                        Text($0)
+        if gameIsActive {
+            NavigationStack {
+                Form {
+                    Section {
+                        if questions.count > 0 {
+                            Text(questions[questionNumber].question)
+                            TextField("Enter your answer", text: $answer)
+                                .keyboardType(.numberPad)
+                        }
+                    }
+                    
+                    Section {
+                        Button("Submit") {
+                            checkAnswer()
+                        }
                     }
                 }
-                
-                Picker("Difficulty", selection: $selectedDifficulty) {
-                    ForEach(difficultyOptions, id: \.self) {
-                        Text($0)
-                    }
-                }
+                .navigationTitle("Question \(questionNumber + 1)")
+                // TODO: Remove this once generateQuestions is working on the other NavigationStack.
+                .onAppear(perform: generateQuestions)
             }
             
-            Section {
-                Button("Start") {
-                    startGame()
+        } else {
+            NavigationStack {
+                Form {
+                    Section {
+                        Stepper(value: $multiplicationTable, in: 2...12) {
+                            Text("Up to \(multiplicationTable) times tables")
+                        }
+                        
+                        Picker("Number of questions", selection: $selectedNumberOfQuestions) {
+                            ForEach(numberOfQuestionsOptions, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        
+                        Picker("Difficulty", selection: $selectedDifficulty) {
+                            ForEach(difficultyOptions, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                    }
+                    
+                    Section {
+                        Button("Start") {
+                            startGame()
+                        }
+                    }
                 }
+                .navigationTitle("Settings")
             }
         }
-        .onAppear(perform: generateQuestions)
     }
     
     func startGame() {
         // TODO: Display the questions based on the selected options.
+        gameIsActive = false
     }
     
     func generateQuestions() {
@@ -61,6 +91,11 @@ struct ContentView: View {
                 questions.append(Question(multiplicationTable: "\(table)", question: "\(number) x \(table)", answer: "\(number * table)"))
             }
         }
+    }
+    
+    func checkAnswer() {
+        print("Checking answer")
+        questionNumber += 1
     }
 }
 
